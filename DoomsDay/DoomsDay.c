6d7fbde914edb019cac12e2fd585efc845da7a82
mbk1991 <mbk1991@next-core.co.kr>
2025-01-15 09:01:37 +0900

    doomsday 수정

--- DoomsDay/DoomsDay.c
+++ DoomsDay/DoomsDay.c
@@ -18,50 +18,36 @@
 //10월: 10일
 //11월: 7일
 //12월: 12일
-
 //년도 별 둠스데이
 //1년 뒤 둠스데이는 하루 뒤 요일
 //윤년인 경우 둠스데이는 이틀 뒤 요일
-
 //2000년도의 둠스데이는 화요일
-
 
 enum Days {MON = 0, TUE = 1, WED = 2, THU = 3, FRI = 4, SAT = 5, SUN = 6};
 int isLeapYear(int year);
 void setDoomsDay();
 int getDayOfWeek(int year, int month, int day);
 void printDay(int d);
-
 short yearDoomsDayArr[3000]; // 0년도 부터 2999년까지 둠스데이
-
 
 int main(){
     setDoomsDay();
-
-
     while(1){
         int year, month, day;
         printf("년 입력(1~2999), exit: -1\n");
         scanf("%d", &year);
-
-
         if(year == -1){
             return 0;
         }
-
         printf("월 입력\n");
         scanf("%d", &month);
         printf("일 입력\n");
         scanf("%d", &day);
-
         int getDay = getDayOfWeek(year, month, day);
         printDay(getDay);
-
     }
-
     return 0;
 }
-
 
 int isLeapYear(int year){
     if( year % 4 == 0 ){
@@ -74,19 +60,15 @@ int isLeapYear(int year){
     }
 }
 
-
 void setDoomsDay(){
     yearDoomsDayArr[2000] = 1; // 2000년도의 둠스데이는 화요일
     for(int i=2001; i<sizeof(yearDoomsDayArr); i++){
-        
         if(isLeapYear(i)){
             yearDoomsDayArr[i] = (yearDoomsDayArr[i-1] + 2) % 7;
         }else{
             yearDoomsDayArr[i] = (yearDoomsDayArr[i-1] + 1) % 7;
         }
     }
-
-
     for(int i=1999; i>0; i--){
         if(isLeapYear(i+1)){
             yearDoomsDayArr[i] = (yearDoomsDayArr[i+1] + (7 - 2)) % 7;
@@ -96,21 +78,19 @@ void setDoomsDay(){
     }
 }
 
-
 int getDayOfWeek(int year, int month, int day){
     if(year < 1 | year > 2999){
         return 0;
     }
-
     int doomsDay = yearDoomsDayArr[year];
-
-    int sameDay = -1;
+    int sameDay;
     switch(month){
         case 1: 
             sameDay = isLeapYear(year)? 25:24;
             break;
         case 2: 
-            sameDay = doomsDay;
+           // sameDay = doomsDay; //sameDay는 날짜의 개념인데 doomsDay는 요일의 개념으로 사용하였었음.
+            sameDay = isLeapYear(year)? 29:28;
             break;
         case 3: 
             sameDay = 7;
@@ -147,24 +127,15 @@ int getDayOfWeek(int year, int month, int day){
     if(day > sameDay){
         int diff = (day - sameDay) % 7;
         return (doomsDay + diff) % 7;
-
-
     }else{
         int diff = (sameDay - day) % 7;
         return (doomsDay + (7-diff)) % 7;
-
-
     }
-
-
-
-
     return -1;
 }
 
 
 void printDay(int d){
-
     printf("=====\n");
     printf("dayOfWeek : ");
     switch(d){
--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,42 +1,87 @@
+import java.math.*;
+
 public class Palindrome{
     public static void main(String[] args){
-	try{
-		gruenBurger(Integer.parseInt(args[0]));
-	}catch(Exception e){
-		System.out.println(e.getMessage());		
-	}
+    	try{
+            if(args[0].length() > 18){
+                BigInteger input = new BigInteger(args[0]);
+                gruenBurger(input);
+         }else{
+	    	gruenBurger(Long.parseLong(args[0]));
+         }
+    	}catch(Exception e){
+    		System.out.println(e.getMessage());		
+    	}
     }
 
     private static void gruenBurger(long num){
-	long reverseNum =  getReverseNum(num);
-	long ans = num + reverseNum;
-	System.out.println("num:" + num);
-	System.out.println("reverse num:" + reverseNum);
-	System.out.println("ans:" + ans);
-	if( isPalindrome(ans) ){
-		System.out.println("Palindrome Number is : " + ans );	
-		System.out.println("---");
-	}else{
-		System.out.println("ans is not Palindrome. continue step... : " + ans );	
-		System.out.println("---");
-		gruenBurger(ans);
-	}
+	    long reverseNum =  getReverseNum(num);
+    	long ans = num + reverseNum;
+    	System.out.println("num:" + num);
+    	System.out.println("reverse num:" + reverseNum);
+    	System.out.println("ans:" + ans);
+    	if( isPalindrome(ans) ){
+    		System.out.println("ans is Palindrome!!! : " + ans );	
+    		System.out.println("---");
+    	}else{
+    		System.out.println("ans is not Palindrome. continue step... : " + ans );	
+    		System.out.println("---");
+    		gruenBurger(ans);
+    	}
+     }
+
+    private static void gruenBurger(BigInteger num){
+        BigInteger reverseNum = getReverseNum(num);
+        BigInteger ans = num.add(reverseNum);
+        System.out.println("num:" + num);
+        System.out.println("reverse num:" + reverseNum);
+        System.out.println("ans:" + ans);
+        if( isPalindrome(ans) ){
+            System.out.println("ans is Palindrome!!! : " + ans );
+            System.out.println("---");
+        }else{
+            System.out.println("ans is not Palindrome. continue step... : " + ans );
+            System.out.println("---");
+            gruenBurger(ans);
+        }
     }
+
+
     private static long getReverseNum(long num){
-	String strNum = num + "";
-	String strReverseNum = new StringBuilder(strNum).reverse().toString();
-	return Long.parseLong(strReverseNum);
+    	String strNum = num + "";
+    	String strReverseNum = new StringBuilder(strNum).reverse().toString();
+    	return Long.parseLong(strReverseNum);
+     }
+
+    private static BigInteger getReverseNum(BigInteger num){
+        String strNum = num.toString();
+        String strReverseNum =new StringBuilder(strNum).reverse().toString();
+        return new BigInteger(strReverseNum);
     }
-    private static boolean isPalindrome(long num){
-	String numStr = num + "";
-	int len = numStr.length();
-	int lastIdx = len - 1;
-	char[] numCharArray = numStr.toCharArray();
-	for(int i=0; i<len; i++){
-	    if(numCharArray[i] != numCharArray[lastIdx-i]){
-		return false;
-	    } 
-	}
-	return true;
+
+     private static boolean isPalindrome(long num){
+    	String numStr = num + "";
+    	int len = numStr.length();
+    	int lastIdx = len - 1;
+    	char[] numCharArray = numStr.toCharArray();
+    	for(int i=0; i<len; i++){
+    	    if(numCharArray[i] != numCharArray[lastIdx-i]){
+    		return false;
+    	    } 
+    	}
+    	return true;
+     }
+
+     private static boolean isPalindrome(BigInteger num){
+        String numStr = num.toString();
+        int len = numStr.length();
+        int lastIdx = len - 1;
+        char[] numCharArray = numStr.toCharArray();
+        for(int i=0; i<len; i++){
+            if(numCharArray[i] != numCharArray[lastIdx-i]){
+                return false;
+             }
+         }
+        return true;
     }
 }
